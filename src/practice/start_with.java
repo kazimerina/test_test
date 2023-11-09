e91437815b85a53ce4a00f9561bd8ec805311d6b
eva <admin@yahoo.com>
2023-11-09 09:01:10 -0600

    test

---
+++ src/practice/start_with.java
@@ -0,0 +1,33 @@
+package practice;
+
+public class start_with {
+
+	public static void main(String[] args) {
+		start_with.dhaka();
+
+		start_with.tx();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+   public static void dhaka() {
+	   String a = "longview city";
+	boolean x = a.endsWith("longview City");
+	   System.out.println(x);
+	   
+	   
+   }
+   public static void tx() {
+	   String a = "longview city";
+	boolean x = a.equals("longview city");
+	   System.out.println(x);
+   
+   
+   } 
+   
+}
